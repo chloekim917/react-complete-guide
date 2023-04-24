f1eb40c19ab5c38f83b70fa743720f8756746ebd
chloekim917 <chloekim917@gmail.com>
2023-04-24 17:50:20 +0900

    day 1 of react relearn done

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,17 @@
-function ExpenseItem() {
-    return <div>Date</div>;
+import ExpenseDate from "./ExpenseDate";
+import "./ExpenseItem.css";
+import Card from "./Card.js";
+
+function ExpenseItem(props) {
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item__description">
+        <h2>{props.title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+    </Card>
+  );
 }
 
 export default ExpenseItem;
